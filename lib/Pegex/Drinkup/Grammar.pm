7f527abdd4ed8e2426020f7a56d95147bd60db31
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-10-06 23:09:51 -0500

    put in some of the hard stuff from DU::RecipeParser

--- lib/Pegex/Drinkup/Grammar.pm
+++ lib/Pegex/Drinkup/Grammar.pm
@@ -8,14 +8,59 @@ sub make_tree {
     '+grammar' => 'drinkup',
     '+toprule' => 'recipe',
     '+version' => '0.0.1',
+    'STAR' => {
+      '.rgx' => qr/(?-xism:\G\*)/
+    },
     'description' => {
       '.rgx' => qr/(?-xism:\G([\s\S]*?)(?!:\r?\n[\ \t]*\*)\s*)/
     },
+    'ingredient' => {
+      '.all' => [
+        {
+          '.rgx' => qr/(?-xism:\G\s*)/
+        },
+        {
+          '.ref' => 'STAR'
+        },
+        {
+          '.rgx' => qr/(?-xism:\G\s*)/
+        },
+        {
+          '.ref' => 'number'
+        },
+        {
+          '.rgx' => qr/(?-xism:\G\s*)/
+        },
+        {
+          '.ref' => 'unit'
+        },
+        {
+          '.rgx' => qr/(?-xism:\G\s*)/
+        },
+        {
+          '.ref' => 'name'
+        }
+      ]
+    },
     'ingredients' => {
-      '.rgx' => qr/(?-xism:\G([\s\S]*))/
+      '+min' => 1,
+      '.ref' => 'ingredient'
+    },
+    'instructions' => {
+      '.all' => [
+        {
+          '.rgx' => qr/(?-xism:\G([\s\S]*))/
+        },
+        {
+          '.ref' => 'PEGEX'
+        }
+      ]
     },
     'name' => {
       '.rgx' => qr/(?-xism:\G(?:\s*\r?\n)?(\S.*?)\s*\r?\n)/
+    },
+    'number' => {
+      '.rgx' => qr/(?-xism:\G((?:0|[1-9][0-9]*)?(?:\.[0-9]+)?(?:\s*\+?\s*[0-9]+\s*\/\s*[0-9]+)?))/
     },
     'recipe' => {
       '.all' => [
@@ -27,8 +72,14 @@ sub make_tree {
         },
         {
           '.ref' => 'ingredients'
+        },
+        {
+          '.ref' => 'instructions'
         }
       ]
+    },
+    'unit' => {
+      '.rgx' => qr/(?-xism:\G(\w+)(?:\s*of)?)/
     }
   }
 }
